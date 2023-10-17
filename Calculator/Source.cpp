4efc50ef9ac64bf323c0c10bbb0777d00bddb5b0
Kolyrew <lutchenkomn555@gmail.com>
2023-10-17 22:21:50 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+class PlugInterface { //интерфейс виртуальной функции
+
+};
+
+class Calculator { //считает основные и определяет нужный плагин
+	
+};
+
+class ConsReader { //читатет строку с консоли
+	
+};
+
+class LoadPlugInSCPP { //загрузка плагинов
+
+};
+
+int main(){
+
+
+	return 0;
+}
